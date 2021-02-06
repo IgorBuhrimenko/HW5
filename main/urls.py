b9b2e1aab1e386100255e2ef738d820194d4f2e1
IgorBuhrimenko <git config --global user.email IgorBuhrimenko@users.noreply.github.com>
2021-02-06 16:41:35 +0200

    DjangoVers1.0

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('student', views.show_student, name='student'),
+    path('lector', views.show_lector, name='lector'),
+    path('group', views.show_group, name='group')
+]
